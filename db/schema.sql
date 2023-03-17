325c473761bacbce844850986f6703e845c22ce6
James Xiong <jxiong783@gmail.com>
2023-03-16 19:02:59 -0700

    added more roles to tables created by mysql

--- db/schema.sql
+++ db/schema.sql
@@ -5,6 +5,7 @@ USE employee_db;
 
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    dept_name VARCHAR(30) NOT NULL,
     name VARCHAR(30) NOT NULL
 );
 
@@ -24,7 +25,8 @@ CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT,
+    role_id INT NOT NULL,
+    manager_id BOOLEAN,
     FOREIGN KEY (role_id)
     REFERENCES role(id)
 );
