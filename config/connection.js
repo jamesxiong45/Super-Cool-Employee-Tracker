deaf1ff2a99c028216a3025b2aae064dded16cd9
James Xiong <jxiong783@gmail.com>
2023-03-16 17:41:55 -0700

    added imports/schema commands to create tables

--- config/connection.js
+++ config/connection.js
@@ -3,9 +3,7 @@ const mysql = require('mysql2');
 const db = mysql.createConnection(
     {
         host: 'localhost',
-        // MySQL username,
         user: process.env.DB_USER,
-        // MySQL password
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME
     },
